e6aa6ea2abbaefd22252f93655c0e03b94091084
Asya <bagin.nst@gmail.com>
2023-07-25 22:23:24 +0300

    feat: set up project, added static files

---
+++ static/categories.js
@@ -0,0 +1,14 @@
+export const categories = [
+    {id: 0,
+    title: 'Task', 
+    icon: './img/task.png'
+    },
+    {id: 1,
+    title: 'Random Thought',
+    icon: './img/randomThought.png'
+    },
+    {id: 2,
+    title: 'Idea', 
+    icon: './img/idea.png'
+    },
+]
